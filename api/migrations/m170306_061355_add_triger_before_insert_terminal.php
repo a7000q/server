e0c2d8f1f556dcee3e6edd50a3139909ea3687e3
Rail <a7000q@gmail.com>
2017-03-06 09:18:44 +0300

    s

---
+++ api/migrations/m170306_061355_add_triger_before_insert_terminal.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+class m170306_061355_add_triger_before_insert_terminal extends Migration
+{
+    public function up()
+    {
+        $sql = '
+           
+           CREATE TRIGGER `insert_terminals` BEFORE INSERT ON `terminals`
+                FOR EACH ROW
+                BEGIN
+                    SET NEW.token = MD5(NEW.name + NEW.id_object);
+                END;
+        ';
+
+        $this->execute($sql);
+    }
+
+    public function down()
+    {
+        $sql = '
+            DROP TRIGGER IF EXISTS `insert_terminals`;
+        ';
+
+        $this->execute($sql);
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
